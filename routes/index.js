0625336f86fca7c82b84e7c04268dedcd52c3410
Ihor Banadiga <banadiga@gmail.com>
2018-11-20 16:02:26 +0200

    swagger added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
 
+const options = {
+    definition: {
+        info: {
+            title: 'Zume orders app', // Title (required)
+            version: '1.0.0', // Version (required)
+        },
+    },
+    // Path to the API docs
+    apis: ['./routes/*.js'],
+};
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerDocument = swaggerJSDoc(options);
+
+
+/**
+ * @swagger
+ *
+ * /:
+ *   get:
+ *     description: Welcome page
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *          description: Welcome message
+ */
 router.get('/', function(req, res) {
     console.log(process.env);
     res.send(`Welcome! This is home page. Host name ${process.env.HOSTNAME}`);
 });
+
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+router.use('/api-docs', swaggerUi.serve);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,127 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * @swagger
+ *
+ * definitions:
+ *   NewUser:
+ *     type: object
+ *     required:
+ *       - username
+ *       - password
+ *     properties:
+ *       username:
+ *         type: string
+ *       password:
+ *         type: string
+ *         format: password
+ *   User:
+ *     allOf:
+ *       - $ref: '#/definitions/NewUser'
+ *       - required:
+ *         - id
+ *       - properties:
+ *         id:
+ *           type: integer
+ *           format: int64
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     description: Returns users
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: users
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/User'
+ */
 router.get('/', function(req, res) {
-    res.send(`Returning users`);
+    res.json([{"id": "1"}]);
 });
 
+/**
+ * @swagger
+ *
+ * /users:
+ *   post:
+ *     description: Creates a user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: user
+ *         description: User object
+ *         in:  body
+ *         required: true
+ *         type: string
+ *         schema:
+ *           $ref: '#/definitions/NewUser'
+ *     responses:
+ *       200:
+ *         description: users
+ *         schema:
+ *           $ref: '#/definitions/User'
+ */
 router.post('/', function(req, res) {
-    res.send(`Creating user`);
+    res.json({"id": "1"});
 });
 
-router.put('/:userId', function(req, res) {
-    res.send(`Updating user ${req.params.userId}`);
+/**
+ * @swagger
+ *
+ * /users/{userId}:
+ *   get:
+ *     summary: Get a user by ID
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: userId
+ *         required: true
+ *         description: The id of the user to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: A single user.
+ *         content:
+ *           application/json:
+ *           schema:
+ *           $ref: '#/definitions/User'
+ *       404:
+ *          description: User with id not exist
+ */
+router.get('/:userId', function(req, res) {
+    res.json({"id": req.param("userId")});
 });
 
+/**
+ * @swagger
+ *
+ * /users/{userId}:
+ *   delete:
+ *     description: Delete user by id
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: userId
+ *         required: true
+ *         description: The id of the user to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *          description: Delete user by id
+ *       404:
+ *          description: User with id not exist
+ */
 router.delete('/:userId', function(req, res) {
-    res.send(`Deleting user ${req.params.userId}`);
+    res.status(204).send();
 });
 
 module.exports = router;
